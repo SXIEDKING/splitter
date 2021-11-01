7fb81aac46e0abee05bb03a0ad7742056821ea13
SaiedIslam <saiedislam61@gmail.com>
2021-11-01 13:56:42 +0000

    quarter

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
 "use strict"
 
-alert("hello")
+
+function split4(){
+     //splits a div into 4 by adding 4 quarters
+     addQuarter(this)
+     addQuarter(this)
+     addQuarter(this)
+     addQuarter(this)
+
+
+
+}    
+
+
+
+function addQuarter(intoDiv){
+
+    let q = document.createElement("div") // create a 'div' element - and hold a 'pointer' to it in the variable 'q'
+    // Add the two classes 'quarter' and 'flexy' to this new element
+    q.classList.add("flexy")
+    q.classList.add("quarter")
+
+    q.addEventListener("click",split4)
+
+    intoDiv.appendChild(q)
+
+
+    
+
+}
+
+document.body.addEventListener("click",split4)
