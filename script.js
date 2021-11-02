17d654402e06312f7afff65654d3cb4f99bac876
SaiedIslam <saiedislam61@gmail.com>
2021-11-02 09:31:49 +0000

    delete

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 "use strict"
 
+function split4(e){
 
-function split4(){
-     //splits a div into 4 by adding 4 quarters
-     addQuarter(this)
-     addQuarter(this)
-     addQuarter(this)
-     addQuarter(this)
-
-    ///rgb() function
-    //Math.random
-
-}    
-
-
-
-function addQuarter(intoDiv){
-
-    let q = document.createElement("div") // create a 'div' element - and hold a 'pointer' to it in the variable 'q'
-    // Add the two classes 'quarter' and 'flexy' to this new element
-    q.classList.add("flexy")
-    q.classList.add("quarter")
-
-    q.addEventListener("click",split4)
-
-    intoDiv.appendChild(q)
-
-
-
+    if (e.shiftKey){
+    //If Shift key is pressed - Do the unsplitting
+        this.parentNode.addEventListener("click", split4)
+        this.parentNode.innerHTML = ''
+}else{
+   //If not then clicking will split a div into 4 by adding 4 quarters
+   addQuarter(this)
+   addQuarter(this)
+   addQuarter(this)
+   addQuarter(this)
 
 }
 
-document.body.addEventListener("click",split4)
+// Event 'capturing and bubbling' 
+    e.stopPropagation() //stop the event from firing again on all the ancestor divs
+}
+
+
+function addQuarter(intoDiv){
+    let q = document.createElement("div") //Creates a div element held by the variable 'q'
+    q.style.backgroundColor = `rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255})`
+    //Adds two classes to the div element
+    q.classList.add("flexy")
+    q.classList.add("quarter")
+
+    q.addEventListener("click", split4) //Whenever we click, it splits into 4 sections
+    intoDiv.appendChild(q)
+}
+
+document.body.addEventListener("click", split4)
